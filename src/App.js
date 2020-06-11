5fe919cfbb64cbc206d25de76d5f0fe993da145f
Dino Tudor <dinotudor@gmail.com>
2020-06-10 22:59:33 -0300

    GET and POST methods working

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(()=>{
+    api.get('repositories').then(response => {
+      setRepositories(response.data);
+    });
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('repositories', {
+      title: 'Repo',
+      url: 'www.github.com',
+      techs: 'React'
+    });
+
+    const repository = response.data;
+
+    setRepositories([...repositories, repository])
   }
 
   async function handleRemoveRepository(id) {
@@ -14,15 +31,9 @@ function App() {
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+          {repositories.map(repo => <li key={repo.id}>{repo.title}</li>)}
       </ul>
-
+      <button onClick={() => handleRemoveRepository(1)}>Remover</button>
       <button onClick={handleAddRepository}>Adicionar</button>
     </div>
   );
